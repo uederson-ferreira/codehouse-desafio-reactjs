f4550c435713fd34a4841f97ddf797ea88258533
Uederson <uedersonferreira@gmail.com>
2024-06-25 08:09:23 -0300

    Entrega 2, menur simples

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
+import React from 'react';
 import './App.css';
+import NavBar from './components/NavBar'; // Certifique-se de que o caminho está correto
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bem-vindo à loja!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
